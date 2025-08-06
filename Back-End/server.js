67c16383a7cd5bcabd3cf9748541a7d5e48917ad
Al zaman <alzamanpoothakkal@gmail.com>
2025-08-06 18:56:38 +0530

    created

---
+++ Back-End/server.js
@@ -0,0 +1,18 @@
+const express= require("express");
+const connectDB = require("./config/db");
+
+
+
+
+
+require("dotenv").config();
+
+const app= express();
+connectDB();
+
+app.use(express.json());
+
+
+app.listen(4000,()=>{
+    console.log("Server is running successfully")
+})
